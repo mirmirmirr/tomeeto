bdac33e7c0d7e4b9b255654b71af272cde0eec19
chickenwang6 <nwang842@gmail.com>
2024-10-27 15:14:55 -0400

    OOP starter code

--- tomeeto-backend/app.py
+++ tomeeto-backend/app.py
@@ -3,6 +3,18 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+
+class Event:
+    def __init__(self, generic_week):
+        self.generic = generic_week
+        
+class SpecificDates(Event):
+    def __init__(self, generic_week, something):
+        super().__init__(generic_week)
+        self.something = something
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
