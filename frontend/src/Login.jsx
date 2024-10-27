5dddb968c95a01da4f226bd25296e6093a942050
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2024-10-27 15:44:48 -0400

    prettier checks

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -10,16 +10,28 @@ export default function Login() {
   };
 
   return (
-    <div className={`relative flex flex-col min-h-screen p-4 ${isDarkMode ? 'bg-[#3E505B]' : 'bg-[#F5F5F5]'}`}>
-      <img src={isDarkMode ? darkLogo : lightLogo} alt="Logo" className="absolute top-4 left-8 w-[9vw] h-auto object-contain" />
-      <button onClick={toggleTheme} className="absolute top-4 right-4 p-2 bg-gray-300 rounded-[40vw] shadow-md hover:bg-gray-400 transition duration-300">
+    <div
+      className={`relative flex flex-col min-h-screen p-4 ${isDarkMode ? 'bg-[#3E505B]' : 'bg-[#F5F5F5]'}`}
+    >
+      <img
+        src={isDarkMode ? darkLogo : lightLogo}
+        alt="Logo"
+        className="absolute top-4 left-8 w-[9vw] h-auto object-contain"
+      />
+      <button
+        onClick={toggleTheme}
+        className="absolute top-4 right-4 p-2 bg-gray-300 rounded-[40vw] shadow-md hover:bg-gray-400 transition duration-300"
+      >
         {isDarkMode ? 'Dark Mode' : 'Light Mode'}
       </button>
       <div className="flex flex-row justify-center items-center mt-[10vh]">
-        
         <div className="flex flex-col items-center justify-center">
           <div className="leading-snug -mt-[10vh]">
-            <span className={`text-huge font-bold ${isDarkMode ? 'text-white' : 'text-[#3E505B]'}`}>Login</span>
+            <span
+              className={`text-huge font-bold ${isDarkMode ? 'text-white' : 'text-[#3E505B]'}`}
+            >
+              Login
+            </span>
           </div>
           <div className="leading-snug -mt-[30.4vh]">
             <span className="text-huge font-bold text-red-500">Login</span>
@@ -31,7 +43,11 @@ export default function Login() {
 
         <div className="w-1/2 flex flex-col items-center space-y-4 p-6">
           <div className="w-[35vw] mb-[20px]">
-            <label className={`block font-bold text-sm ${isDarkMode ? 'text-white' : 'text-black'}`}>Username</label>
+            <label
+              className={`block font-bold text-sm ${isDarkMode ? 'text-white' : 'text-black'}`}
+            >
+              Username
+            </label>
             <input
               type="text"
               placeholder="enter username here"
@@ -44,7 +60,11 @@ export default function Login() {
           </div>
 
           <div className="w-[35vw]">
-            <label className={`block font-bold text-sm ${isDarkMode ? 'text-white' : 'text-black'}`}>Password</label>
+            <label
+              className={`block font-bold text-sm ${isDarkMode ? 'text-white' : 'text-black'}`}
+            >
+              Password
+            </label>
             <input
               type="text"
               placeholder="enter password here"
@@ -63,24 +83,31 @@ export default function Login() {
             </div>
           </div>
 
-          <button className={`w-[35vw] text-responsive py-3 font-semibold rounded-lg transition duration-300 ${isDarkMode ? 'bg-white text-[#3E505B]' : 'bg-[#3E505B] text-white'}`}>
+          <button
+            className={`w-[35vw] text-responsive py-3 font-semibold rounded-lg transition duration-300 ${isDarkMode ? 'bg-white text-[#3E505B]' : 'bg-[#3E505B] text-white'}`}
+          >
             Login
           </button>
-          <p className={`w-[35vw] subtext-responsive text-opacity-70 text-center ${isDarkMode ? 'text-white' : 'text-black'} hover:underline hover:text-opacity-100` }>Signup</p>
-          <div className={`w-[35vw] border-t-2 my-4 ${
-                isDarkMode
-                  ? 'border-gray-300'
-                  : 'border-gray-500'
-              }`}></div>
-          <button className={`w-[35vw] mt-[10px] subtext-responsive py-3 rounded-md transition duration-300 hover:bg-red-500 hover:text-white hover:border-transparent ${
-                isDarkMode
-                  ? 'text-white bg-transparent border-2 border-white'
-                  : 'text-back bg-transparent border-2 border-[#3E505B]'
-              }`}>
+          <p
+            className={`w-[35vw] subtext-responsive text-opacity-70 text-center ${isDarkMode ? 'text-white' : 'text-black'} hover:underline hover:text-opacity-100`}
+          >
+            Signup
+          </p>
+          <div
+            className={`w-[35vw] border-t-2 my-4 ${
+              isDarkMode ? 'border-gray-300' : 'border-gray-500'
+            }`}
+          ></div>
+          <button
+            className={`w-[35vw] mt-[10px] subtext-responsive py-3 rounded-md transition duration-300 hover:bg-red-500 hover:text-white hover:border-transparent ${
+              isDarkMode
+                ? 'text-white bg-transparent border-2 border-white'
+                : 'text-back bg-transparent border-2 border-[#3E505B]'
+            }`}
+          >
             Continue with Google
           </button>
         </div>
-        
       </div>
     </div>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     fontSize: {
-      'tiny': '0.625rem', 
-      'huge': '20vw',
+      tiny: '0.625rem',
+      huge: '20vw',
     },
   },
   plugins: [],
-}
+};
