a97c1925ddcbf79eabd58d99c465547a0b0e41aa
chickenwang6 <nwang842@gmail.com>
2024-11-01 16:25:51 -0400

    basic backend structure

---
+++ backend/app.py
@@ -0,0 +1,45 @@
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# class Event:
+#     def __init__(self, generic_week):
+#         self.generic = generic_week
+
+#     # user event availability object
+#     # store as a list of objects for availabilites ([Monday 11/5, Tuesday 11/6, ...])
+        
+# class SpecificDates(Event):
+#     def __init__(self, generic_week, something):
+#         super().__init__(generic_week)
+#         self.something = something
+
+# class User:
+#     def __init__(self):
+
+# class AccountUser(User):
+#     def __init__(self):
+#         super().__init__()
+
+
+# Date Range, Time Range, Generic Event (?)
+
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/create_event")
+async def create_event():
+    return {"message": "you sent calendar data"}
+
+@app.get("/login")
+async def login():
+    return {"message": "you sent login data"}
+
+@app.get("/cookies")
+async def root():
+    return {"message": "you sent cookie data"}
