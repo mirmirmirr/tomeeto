f3fa2ea0922b51f07ab383519599f3ad1f38868e
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2024-11-01 21:15:00 -0400

    css for results page

--- frontend/src/Results.jsx
+++ frontend/src/Results.jsx
@@ -6,8 +6,9 @@ export default function Result() {
   const [isDarkMode, setIsDarkMode] = useState(true);
   const [hoveredCell, setHoveredCell] = useState({ row: null, column: null });
 
-  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  const hours = Array.from({ length: 11 }, (_, i) => 7 + i);
+  const eventTitle = "tomeeto planning"
+  const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+  const hours = Array.from({ length: 15 }, (_, i) => 7 + i);
   const scheduleData = [
     {
       name: 'Alice',
@@ -19,6 +20,10 @@ export default function Result() {
         [0, 0, 0, 0, 1, 0, 0], // 11 AM
         [0, 0, 0, 1, 1, 0, 0], // 12 PM
         [0, 0, 0, 0, 0, 0, 0], // 1 PM
+        [1, 1, 1, 1, 1, 1, 1], // 2 PM
+        [0, 0, 0, 0, 0, 0, 0], // 3 PM
+        [1, 0, 1, 0, 0, 0, 0], // 4 PM
+        [0, 0, 0, 0, 0, 0, 0], // 5 PM
         [1, 1, 1, 1, 1, 1, 1], // 2 PM
         [0, 0, 0, 0, 0, 0, 0], // 3 PM
         [1, 0, 1, 0, 0, 0, 0], // 4 PM
@@ -38,6 +43,10 @@ export default function Result() {
         [1, 1, 1, 1, 1, 1, 1], // 2 PM
         [0, 0, 0, 0, 0, 0, 0], // 3 PM
         [0, 0, 0, 0, 0, 0, 0], // 4 PM
+        [0, 0, 0, 0, 0, 0, 0], // 5 PM
+        [1, 1, 1, 1, 1, 1, 1], // 2 PM
+        [0, 0, 0, 0, 0, 0, 0], // 3 PM
+        [1, 0, 1, 0, 0, 0, 0], // 4 PM
         [0, 0, 0, 0, 0, 0, 0], // 5 PM
       ],
     },
@@ -69,25 +78,30 @@ export default function Result() {
       >
         {isDarkMode ? 'Dark Mode' : 'Light Mode'}
       </button>
-      <div className="flex flex-col mt-[10vh]">
-        <div id='eventName'>tomeeto planning</div>
+      <div className={`flex flex-col mt-[7vh] p-4 ${isDarkMode ? 'text-white' : 'text-black'}`}>
+        <div  id='eventName' 
+              className='pl-4'
+              style={{ fontSize: `min(3vw, 60px)` }}
+        >
+          {eventTitle}
+        </div>
 
-        <div className={`w-[35vw] border-t-2 my-4 ${ isDarkMode ? 'border-gray-300' : 'border-gray-500'}`}></div>
+        <div className={`w-[90vw] border-t-2 ml-4 opacity-25 ${ isDarkMode ? 'border-gray-300' : 'border-gray-500'}`}></div>
 
-        <div id='results' className='flex flexrow'>
-          <table className='w-[70vw]'>
+        <div id='results' className='w-[80vw] h-[70vh] ml-[5%] mr-auto mt-[3vh] flex flex-row overflow-hidden'>
+          <table className='w-[80%] h-[70%] table-fixed'>
             <thead>
                 <tr>
-                <th className="border border-gray-300 p-2"></th>
+                <th className="p-2 w-[10%]"></th>
                   {days.map((day, index) => (
-                    <th key={index} className="border border-gray-300 p-2">{day}</th>
+                    <th key={index} className="p-2 font-[400]">{day}</th>
                   ))}
                 </tr>
             </thead>
             <tbody>
               {hours.map((hour, row) => (
-                <tr key={row}>
-                  <td className="border border-gray-300 p-2">{hour}:00 {hour < 12 ? 'AM' : 'PM'}</td>
+                <tr key={row} className="h-full" style={{ height: `calc(100% / ${hours.length})` }}>
+                  <td className="p-2 text-right text-[10pt]">{hour <= 12 ? hour : hour - 12} {hour < 12 ? 'AM' : 'PM'}</td>
                   {days.map((_,column) => {
                     const availability = availabilityCounts[row][column];
                     const opacity = availability > 0 ? availability : 0;
@@ -95,9 +109,10 @@ export default function Result() {
                     return (
                       <td 
                         key={column}
-                        className="border border-gray-300 p-2"
+                        className="border border-gray-400 p-2"
                         style={{
                           backgroundColor:`rgba(72, 187, 120, ${opacity})`,
+                          border: '1px solid #b9b9b9',
                         }}
 
                         onMouseEnter={() => setHoveredCell({ row, column })}
@@ -110,14 +125,14 @@ export default function Result() {
             </tbody>
           </table>
 
-          <div className='flex flex-col ml-auto mr-auto items-center'>
-            <div>Attendees ({scheduleData.length})</div>
+          <div className='w-[25%] flex flex-col ml-auto mr-auto items-center'>
+            <div className='text-[23px] font-[500] p-5' >Attendees ({scheduleData.length})</div>
             {scheduleData.map((attendee, index) => {
               const isAvailable = hoveredCell.row !== null && hoveredCell.column !== null && attendee.availability[hoveredCell.row][hoveredCell.column] == 1;
               const opacityStyle = isAvailable ? 'opacity-100' : 'opacity-50 line-through';
 
               return (
-                <div key={index} className={`transition duration-300 ${opacityStyle}`}>
+                <div key={index} className={`text-[19px] font-[300] transition duration-300 ${opacityStyle}`}>
                   {attendee.name}
                 </div>
               );
