20c650dd1e052d2c753bf2b11a222829a1724c37
chickenwang6 <nwang842@gmail.com>
2024-11-02 23:07:43 -0400

    hashing functions and link generators with basic api endpoints

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 import base64
+import random
+import string
 
 app = FastAPI()
 
@@ -25,11 +27,8 @@ app = FastAPI()
 
 # Date Range, Time Range, Generic Event (?)
 
-
-
-# Function to Hash a Password:
+# Function to hash a password and decode it for security purposes:
 def encode_string(input_string: str) -> str:
-    # Encode the string in Base64
     encoded_bytes = base64.b64encode(input_string.encode())
     return encoded_bytes.decode()
 
@@ -37,6 +36,14 @@ def decode_string(encoded_string: str) -> str:
     # Decode the Base64 encoded string
     decoded_bytes = base64.b64decode(encoded_string.encode())
     return decoded_bytes.decode()
+
+# Function to generate a random link for the user once an event is made
+def generate_random_link(length: int = 10) -> str:
+    # Define the characters to use: uppercase, lowercase letters, and digits
+    characters = string.ascii_letters + string.digits
+    # Randomly select 'length' characters from the character set
+    random_link = ''.join(random.choice(characters) for _ in range(length))
+    return "http://tomeeto.cs.rpi.edu/" + random_link
 
 
 @app.get("/")
@@ -46,6 +53,19 @@ async def root():
 
 @app.get("/create_event")
 async def create_event():
+    rows = 0
+    cols = 0
+    calendar_data = {
+        row: rows,
+        col: cols,
+        free_times: []
+    }
+
+    for row in range(rows):
+        for col in range(cols):
+            if painting[row][col] != 0:
+                calendar_data[free_times].append((row, col))
+
     return {"message": "you sent calendar data"}
 
 
