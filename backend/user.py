49bcc6e4d028225a5ea7cab1225b0246d152ae52
jzgom067 <jzgombic3@gmail.com>
2024-11-03 02:03:57 -0500

    Add events to Users
    Might be smart to have references back and forth.

--- backend/user.py
+++ backend/user.py
@@ -1,6 +1,14 @@
+from typing import List
+from event import Event
+
+
 class User:
     def __init__(self, id: int) -> None:
         self.id: int = id
+        self.events: List[Event] = []
+
+    def add_event(self, event: Event) -> None:
+        self.events.append(event)
 
 
 class AccountUser(User):
