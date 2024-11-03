b50944a84b06561c7977415ba4138b0bbb619cde
jzgom067 <jzgombic3@gmail.com>
2024-11-03 17:11:13 -0500

    Change user_account table for guests
    Guest status should be specified, and email and password shouldn't be required.

--- database/db_setup.sql
+++ database/db_setup.sql
@@ -10,10 +10,10 @@ DROP TABLE IF EXISTS user_account;
 
 CREATE TABLE IF NOT EXISTS user_account (
     user_account_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
+    email VARCHAR(255),
+    password_hash VARCHAR(255),
     is_internal BOOL NOT NULL DEFAULT FALSE,
-    is_guest BOOL NOT NULL DEFAULT TRUE,
+    is_guest BOOL NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) COMMENT 'Accounts for users';
 
