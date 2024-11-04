b7baa47af62e8ecf2f7b19fcb1e2a5f44a9cb517
jzgom067 <jzgombic3@gmail.com>
2024-11-03 20:23:02 -0500

    Remove class mapping from requests
    I'd much rather work with direct JSON than brute force an object structure into our current system.

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Request, FastAPI
 from utils import (
     db_hello_world,
     check_user_exists,
@@ -6,7 +6,6 @@ from utils import (
     add_user,
     check_login,
 )
-from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -16,7 +15,32 @@ async def root():
     return db_hello_world()
 
 
-@app.get("/create_event")
+@app.post("/signup")
+async def sign_up(request: Request):
+    info = await request.json()
+    email = info["email"]
+    password = info["password"]
+
+    result: str = check_user_exists(email)
+    if len(result) > 0:
+        return {"message": result}
+    pass_hash = hash_new_password(password)
+    add_user(email, pass_hash)
+    return {"message": "User created"}
+
+
+@app.post("/login")
+async def login(request: Request):
+    info = await request.json()
+    email = info["email"]
+    password = info["password"]
+
+    if check_login(email, password):
+        return {"message": "Login successful"}
+    return {"message": "Login failed"}
+
+
+@app.post("/create_event")
 async def create_event():
     rows = 0
     cols = 0
@@ -27,26 +51,4 @@ async def create_event():
             if painting[row][col] != 0:
                 calendar_data[free_times].append((row, col))
 
-    return {"message": "you sent calendar data"}
-
-
-class AuthInfo(BaseModel):
-    email: str
-    password: str
-
-
-@app.post("/signup")
-async def sign_up(info: AuthInfo):
-    result: str = check_user_exists(info.email)
-    if len(result) > 0:
-        return {"message": result}
-    pass_hash = hash_new_password(info.password)
-    add_user(info.email, pass_hash)
-    return {"message": "User created"}
-
-
-@app.post("/login")
-async def login(info: AuthInfo):
-    if check_login(info.email, info.password):
-        return {"message": "Login successful"}
-    return {"message": "Login failed"}
+    return {"message": "event created"}
