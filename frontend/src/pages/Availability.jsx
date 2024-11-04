f8d09ef214b4b9d9f6e083faec916c3fd7f1fccb
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2024-11-04 14:22:01 -0500

    multi page date functionality added

--- frontend/src/pages/Availability.jsx
+++ frontend/src/pages/Availability.jsx
@@ -8,12 +8,50 @@ export default function Availability() {
 
   const { isDarkMode, toggleTheme } = useTheme();
   const [availability, setAvailability] = useState(
-    Array(7).fill(Array(24).fill(false))
+    Array(11).fill(Array(24).fill(false))
   );
   const [isDragging, setIsDragging] = useState(false);
   const [dragStart, setDragStart] = useState(null);
   const [dragEnd, setDragEnd] = useState(null);
   const [name, setName] = useState('');
+
+  const [currentPage, setCurrentPage] = useState(0);
+  const [isDisabled] = useState(true);
+  const daysPerPage = 7;
+
+  const eventTitle = 'tomeeto planning';
+  const allDays = [
+    'SUN 6',
+    'MON 7',
+    'TUE 8',
+    'WED 9',
+    'THU 10',
+    'FRI 11',
+    'SAT 12',
+    'SUN 13',
+    'MON 14',
+    'TUE 15',
+    'WED 16',
+  ];
+  const totalDays = allDays.length; // Total number of days
+  const hours = Array.from({ length: 15 }, (_, i) => 7 + i);
+
+  const displayedDays = allDays.slice(
+    currentPage * daysPerPage,
+    (currentPage + 1) * daysPerPage
+  );
+
+  const goToPrevPage = () => {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const goToNextPage = () => {
+    if ((currentPage + 1) * daysPerPage < totalDays) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
 
   const handleMouseDown = (day, hour) => {
     setIsDragging(true);
@@ -79,36 +117,6 @@ export default function Availability() {
     );
   };
 
-  /*
-  const handleSubmit = async () => {
-    const data = {
-      name,
-      availability,
-    };
-
-    try {
-      const response = await fetch('YOUR_BACKEND_URL', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      });
-
-      if (response.ok) {
-        console.log('Data submitted successfully');
-      } else {
-        console.error('Failed to submit data');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
-  */
-
-  const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  const hoursOfDay = Array.from({ length: 15 }, (_, i) => 7 + i);
-
   return (
     <div
       className={`relative flex flex-col min-h-screen p-4 ${isDarkMode ? 'bg-[#3E505B]' : 'bg-[#F5F5F5]'}`}
@@ -124,11 +132,11 @@ export default function Availability() {
           className="pl-4"
           style={{ fontSize: `min(3vw, 60px)` }}
         >
-          tomeeto planning
+          {eventTitle}
         </div>
 
         <div
-          className={`w-full border-t-2 ml-4 opacity-25 ${isDarkMode ? 'border-gray-300' : 'border-gray-500'}`}
+          className={`w-[90vw] border-t-2 ml-4 opacity-25 ${isDarkMode ? 'border-gray-300' : 'border-gray-500'}`}
         ></div>
 
         <div className="flex justify-between items-center mt-4">
@@ -154,51 +162,95 @@ export default function Availability() {
 
         <div
           id="availability"
-          className="w-full h-[70vh] ml-auto mr-auto mt-[3vh] flex flex-col overflow-hidden"
+          className="w-[90vw] h-[70vh] mt-[3vh] flex flex-row overflow-hidden"
         >
-            <table className="w-full table-fixed">
-              <thead>
-                <tr>
-                  <th className="p-2" style={{ width: `min(10vw, 55px)` }}></th>
-                  {daysOfWeek.map((day, index) => (
-                    <th key={index} className="p-2 font-[400]">
-                      {day}
-                    </th>
+          <div className="flex mt-4">
+            {currentPage > 0 && (
+              <button
+                onClick={goToPrevPage}
+                className="px-4 py-2 font-bold opacity-25 hover:opacity-100"
+                style={{ fontSize: '2rem' }}
+              >
+                &#65308; {/* Previous page entity */}
+              </button>
+            )}
+            {(currentPage + 1) * daysPerPage < totalDays && (
+              <button
+                onClick={goToNextPage}
+                className="h-full flex items-center justify-center px-4 py-2 opacity-0"
+                style={{ fontSize: '2rem' }}
+                disabled={isDisabled}
+              >
+                &#65310;
+              </button>
+            )}
+          </div>
+
+          <table className="w-full table-fixed">
+            <thead>
+              <tr>
+                <th className="p-2" style={{ width: `min(10vw, 55px)` }}></th>
+                {displayedDays.map((day, index) => (
+                  <th key={index} className="p-2 font-[400]">
+                    {day}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {hours.map((hour, row) => (
+                <tr
+                  key={row}
+                  className="h-full"
+                  style={{ height: `calc(100% / ${hours.length})` }}
+                >
+                  <td className="p-2 text-right text-[10pt]">
+                    {hour <= 12 ? hour : hour - 12} {hour < 12 ? 'AM' : 'PM'}
+                  </td>
+                  {displayedDays.map((_, column) => (
+                    <td
+                      key={column}
+                      className={`border ${isDarkMode ? 'border-white' : 'border-black'}`}
+                      style={{
+                        backgroundColor: availability[column][hour]
+                          ? 'rgba(72, 187, 120, 1)'
+                          : isInDragArea(column, hour)
+                            ? availability[dragStart.day][dragStart.hour]
+                              ? 'rgba(255, 0, 0, 0.5)' // Preview for unfill
+                              : 'rgba(72, 187, 120, 0.5)' // Preview for fill
+                            : 'transparent',
+                      }}
+                      onMouseDown={() => handleMouseDown(column, hour)}
+                      onMouseEnter={() => handleMouseEnter(column, hour)}
+                      onClick={() => handleClick(column, hour)}
+                    ></td>
                   ))}
                 </tr>
-              </thead>
-              <tbody>
-                {hoursOfDay.map((hour, row) => (
-                  <tr
-                    key={row}
-                    className="h-full"
-                    style={{ height: `calc(100% / ${hoursOfDay.length})` }}
-                  >
-                    <td className="p-2 text-right text-[10pt]">
-                      {hour <= 12 ? hour : hour - 12} {hour < 12 ? 'AM' : 'PM'}
-                    </td>
-                    {daysOfWeek.map((_, column) => (
-                      <td
-                        key={column}
-                        className={`border ${isDarkMode ? 'border-white' : 'border-black'}`}
-                        style={{
-                          backgroundColor: availability[column][hour]
-                            ? 'rgba(72, 187, 120, 1)'
-                            : isInDragArea(column, hour)
-                              ? availability[dragStart.day][dragStart.hour]
-                                ? 'rgba(255, 0, 0, 0.5)' // Preview for unfill
-                                : 'rgba(72, 187, 120, 0.5)' // Preview for fill
-                              : 'transparent',
-                        }}
-                        onMouseDown={() => handleMouseDown(column, hour)}
-                        onMouseEnter={() => handleMouseEnter(column, hour)}
-                        onClick={() => handleClick(column, hour)}
-                      ></td>
-                    ))}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+              ))}
+            </tbody>
+          </table>
+
+          <div className="flex justify-between mt-4">
+            {currentPage > 0 && (
+              <button
+                onClick={goToPrevPage}
+                className="px-4 py-2 opacity-0"
+                style={{ fontSize: '2rem' }}
+                disabled={isDisabled}
+              >
+                &#65308; {/* Previous page entity */}
+              </button>
+            )}
+            {(currentPage + 1) * daysPerPage < totalDays && (
+              <button
+                onClick={goToNextPage}
+                className="h-full flex items-center justify-center px-4 py-2 font-bold opacity-25 hover:opacity-100"
+                style={{ fontSize: '2rem' }}
+              >
+                &#65310; {/* Next page entity */}
+              </button>
+            )}
+          </div>
         </div>
       </div>
     </div>
