489b55d3a17827680aecec248e2a21f04577b32f
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2024-11-19 18:07:19 -0500

    event created confirmed mobile

--- src/pages/ConfirmCreated.jsx
+++ src/pages/ConfirmCreated.jsx
@@ -16,7 +16,7 @@ export default function ConfirmCreated() {
       .writeText(eventCode)
       .then(() => {
         setCopySuccess(true);
-        setTimeout(() => setCopySuccess(false), 2000); // Show success message briefly
+        setTimeout(() => setCopySuccess(false), 2000);
       })
       .catch((err) => {
         console.error('Failed to copy text: ', err);
@@ -35,66 +35,72 @@ export default function ConfirmCreated() {
 
   return (
     <div
-      className={`relative flex flex-col items-start justify-center min-h-screen
-        p-4 ${bgColor}`}
+      className={`relative flex flex-col items-start justify-center min-h-[90vh] lg:min-h-screen p-4 ${bgColor}`}
     >
       <Header isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
-      <div className="flex w-full align-center">
+      <div className="flex flex-col lg:flex-row w-full align-center lg:m-0 m-[10px]">
         <div>
-          <h1 className={`text-[15vw] leading-[25vh] font-bold ${textColor}`}>
+          <h1 className={`text-[15vw] leading-[7vh] lg:leading-[25vh] font-bold ${textColor}`}>
             Event Created!
           </h1>
 
-          <p className={`text-[2vw] leading-[10vh] ml-[10vw] ${textColor}`}>
-            (Your event will be deleted after two weeks inactivity)
+          <p className={`text-[20px] mt-[10px] lg:text-[2vw] lg:text-center ${textColor}`}>
+            (Your event will be deleted after <b>two weeks</b> inactivity)
           </p>
         </div>
 
         <div className="flex flex-col items-center align-center justify-end space-y-4 mr-[5vw]">
-          <div id="password" className="w-full relative">
+          <div className="w-[60vw] lg:w-full relative">
             <label
-              className={`mt-[30px] mb-[0px] block text-[1vw] ${textColor} flex flex-row`}
+              className={`mt-[30px] mb-[0px] block text-[20px] lg:text-[1vw] ${textColor} flex flex-row`}
             >
               EVENT CODE
               {copySuccess && (
                 <p
-                  className={`text-[0.8vw] absolute right-[90px] ${textColor}`}
+                  className={`text-[12px] lg:text-[0.8vw] absolute right-0 lg:right-[100px] ${textColor}`}
                 >
                   hazzah!
                 </p>
               )}
             </label>
-            <div className="w-full mt-[-10px] py-2 flex items-center relative group">
+            <div onClick={handleCopy} className="w-[60vw] lg:w-full mt-[-10px] py-2 flex items-center relative group">
               <p
                 className={`flex-1 text-responsive py-2 font-bold bg-transparent border-b-2 rounded-none focus:outline-none ${isDarkMode ? 'text-white border-white' : 'text-black border-black'}`}
               >
                 {eventCode}
               </p>
+
               <button
                 onClick={handleCopy}
-                className={`absolute text-[0.8vw] right-0 bottom-[55px] py-1 px-4 cursor-pointer rounded-lg opacity-0 group-hover:opacity-100 transition-opacity ${buttonText} ${buttonBG}`}
+                className={`hidden lg:block absolute text-[0.8vw] right-0 bottom-[55px] py-1 px-4 cursor-pointer rounded-lg opacity-0 group-hover:opacity-100 transition-opacity ${buttonText} ${buttonBG}`}
               >
                 copy link
               </button>
               <button
                 onClick={() => navigate('/availability')}
-                className={`absolute text-[0.8vw] right-0 bottom-[20px] py-1 px-4 cursor-pointer rounded-lg opacity-0 group-hover:opacity-100 transition-opacity ${buttonText} ${buttonBG}`}
+                className={`hidden lg:block absolute text-[0.8vw] right-0 bottom-[20px] py-1 px-4 cursor-pointer rounded-lg opacity-0 group-hover:opacity-100 transition-opacity ${buttonText} ${buttonBG}`}
               >
                 fill availability
               </button>
             </div>
           </div>
 
-          <div className="flex flex-row">
+          <div className="flex flex-col lg:flex-row">
+            <button
+              onClick={() => navigate('/availability')}
+              className={`lg:hidden w-[60vw] lg:w-[13vw] m-[10px] bg-transparent border-2 text-responsive py-2 font-semibold rounded-lg ${textColor} ${borderColor} hover:bg-red-500 hover:text-white hover:border-transparent`}
+            >
+              Add Availability
+            </button>
             <button
               onClick={() => navigate('/create')}
-              className={`w-[13vw] m-[10px] bg-transparent border-2 text-responsive py-2 font-semibold rounded-lg ${textColor} ${borderColor} hover:bg-red-500 hover:text-white hover:border-transparent`}
+              className={`w-[60vw] lg:w-[13vw] m-[10px] bg-transparent border-2 text-responsive py-2 font-semibold rounded-lg ${textColor} ${borderColor} hover:bg-red-500 hover:text-white hover:border-transparent`}
             >
               Edit Event
             </button>
             <button
               onClick={() => navigate('/results')}
-              className={`w-[13vw] m-[10px] text-responsive py-2 font-semibold rounded-lg ${buttonText} ${buttonBG}  hover:bg-red-500 hover:text-white hover:border-transparent`}
+              className={`w-[60vw] lg:w-[13vw] m-[10px] text-responsive py-2 font-semibold rounded-lg ${buttonText} ${buttonBG}  hover:bg-red-500 hover:text-white hover:border-transparent`}
             >
               Results
             </button>
