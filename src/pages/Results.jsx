ebe419de475164df7d1461cd29dabfedf5b951b9
Gavin Liu <85533492+liug88@users.noreply.github.com>
2024-11-19 12:42:06 -0500

    Results Mobile implmentation
    Results page now looks good on mobile, and also now shows a card on each individual cell showing availability when clicked

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,16 +1,15 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useTheme } from '../resources/ThemeContext';
-
 import Header from '../resources/Header';
 
 export default function Result() {
   const navigate = useNavigate();
-
   const { isDarkMode, toggleTheme } = useTheme();
   const [hoveredCell, setHoveredCell] = useState({ row: null, column: null });
   const [currentPage, setCurrentPage] = useState(0);
   const [isDisabled] = useState(true);
+  const [selectedCell, setSelectedCell] = useState(null);
   const daysPerPage = 7; // Number of days per page
 
   const eventTitle = 'tomeeto planning';
@@ -22,69 +21,13 @@ export default function Result() {
     'THU 10',
     'FRI 11',
     'SAT 12',
-    'SUN 13',
-    'MON 14',
-    'TUE 15',
-    'WED 16',
   ];
+
   const totalDays = allDays.length; // Total number of days
   const hours = Array.from({ length: 15 }, (_, i) => 7 + i);
-
   const displayedDays = allDays.slice(
     currentPage * daysPerPage,
     (currentPage + 1) * daysPerPage
-  );
-
-  const scheduleData = [
-    {
-      name: 'Alice',
-      availability: [
-        [0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0], // 7 AM
-        [1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0], // 8 AM
-        [0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0], // 9 AM
-        [0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0], // 10 AM
-        [0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0], // 11 AM
-        [0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0], // 12 PM
-        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0], // 1 PM
-        [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0], // 2 PM
-        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0], // 3 PM
-        [1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0], // 4 PM
-        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0], // 5 PM
-        [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0], // 2 PM
-        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0], // 3 PM
-        [1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0], // 4 PM
-        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0], // 5 PM
-      ],
-    },
-    {
-      name: 'Bob',
-      availability: [
-        [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0], // 8 AM
-        [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0], // 7 AM
-        [0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0], // 9 AM
-        [1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0], // 10 AM
-        [1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0], // 11 AM
-        [0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0], // 12 PM
-        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0], // 1 PM
-        [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0], // 2 PM
-        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0], // 3 PM
-        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0], // 4 PM
-        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0], // 5 PM
-        [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0], // 2 PM
-        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0], // 3 PM
-        [1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0], // 4 PM
-        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0], // 5 PM
-      ],
-    },
-  ];
-
-  const availabilityCounts = hours.map((_, row) =>
-    displayedDays.map((_, column) => {
-      const count = scheduleData.filter(
-        (attendee) => attendee.availability[row][column] == 1
-      ).length;
-      return count / scheduleData.length;
-    })
   );
 
   const goToPrevPage = () => {
@@ -97,6 +40,14 @@ export default function Result() {
     if ((currentPage + 1) * daysPerPage < totalDays) {
       setCurrentPage(currentPage + 1);
     }
+  };
+
+  const handleCellClick = (row, column) => {
+    setSelectedCell({ row, column });
+  };
+
+  const closeModal = () => {
+    setSelectedCell(null);
   };
 
   return (
@@ -114,16 +65,19 @@ export default function Result() {
         >
           {eventTitle}
         </div>
-
         <div
-          className={`w-[90vw] border-t-2 ml-4 opacity-25 ${isDarkMode ? 'border-gray-300' : 'border-gray-500'}`}
+          className={`w-[93vw] border-t-2 m-auto opacity-25 ${isDarkMode ? 'border-gray-300' : 'border-gray-500'}`}
         ></div>
-
+        <div className="flex w-[90vw] justify-between items-center mt-4">
+          <div className="pl-4" style={{ fontSize: `min(2vw, 20px)` }}>
+            Results
+          </div>
+        </div>
         <div
-          id="results"
-          className="w-[80vw] h-[70vh] ml-[5%] mr-auto mt-[3vh] flex flex-row overflow-hidden"
+          id="availability"
+          className="w-[90vw] h-[64vh] mt-[1vh] flex flex-row overflow-hidden"
         >
-          <div className="flex justify-between mt-4">
+          <div className="flex mt-4">
             {currentPage > 0 && (
               <button
                 onClick={goToPrevPage}
@@ -144,8 +98,7 @@ export default function Result() {
               </button>
             )}
           </div>
-
-          <table className="w-[80%] table-fixed">
+          <table className="w-full table-fixed">
             <thead>
               <tr>
                 <th className="p-2" style={{ width: `min(10vw, 55px)` }}></th>
@@ -163,80 +116,52 @@ export default function Result() {
                   className="h-full"
                   style={{ height: `calc(100% / ${hours.length})` }}
                 >
-                  <td className="text-right text-[10pt]">
+                  <td className="pr-2 text-right text-[10pt]">
                     {hour <= 12 ? hour : hour - 12} {hour < 12 ? 'AM' : 'PM'}
                   </td>
-                  {displayedDays.map((_, column) => {
-                    const availability = availabilityCounts[row][column];
-                    const opacity = availability > 0 ? availability : 0;
-
-                    return (
-                      <td
-                        key={column}
-                        className="border border-gray-400 p-2"
-                        style={{
-                          backgroundColor: `rgba(72, 187, 120, ${opacity})`,
-                          border: '1px solid #b9b9b9',
-                        }}
-                        onMouseEnter={() => setHoveredCell({ row, column })}
-                        onMouseLeave={() =>
-                          setHoveredCell({ row: null, column: null })
-                        }
-                      ></td>
-                    );
-                  })}
+                  {displayedDays.map((_, column) => (
+                    <td
+                      key={column}
+                      className={`border ${isDarkMode ? 'border-white' : 'border-black'}`}
+                      style={{
+                        backgroundColor:
+                          hoveredCell.row === row &&
+                          hoveredCell.column === column
+                            ? 'rgba(72, 187, 120, 0.5)'
+                            : 'transparent',
+                        userSelect: 'none',
+                      }}
+                      onMouseEnter={() => setHoveredCell({ row, column })}
+                      onMouseLeave={() =>
+                        setHoveredCell({ row: null, column: null })
+                      }
+                      onClick={() => handleCellClick(row, column)}
+                    ></td>
+                  ))}
                 </tr>
               ))}
             </tbody>
           </table>
-
-          <div className="flex justify-between mt-4">
-            {currentPage > 0 && (
-              <button
-                onClick={goToPrevPage}
-                className="px-4 py-2 opacity-0"
-                style={{ fontSize: '2rem' }}
-                disabled={isDisabled}
-              >
-                &#65308; {/* Previous page entity */}
-              </button>
-            )}
-            {(currentPage + 1) * daysPerPage < totalDays && (
-              <button
-                onClick={goToNextPage}
-                className="h-full flex items-center justify-center px-4 py-2 font-bold opacity-25 hover:opacity-100"
-                style={{ fontSize: '2rem' }}
-              >
-                &#65310; {/* Next page entity */}
-              </button>
-            )}
-          </div>
-
-          <div className="w-[25%] flex flex-col ml-auto mr-auto items-center">
-            <div className="text-[23px] font-[500] p-5">
-              Attendees ({scheduleData.length})
-            </div>
-            {scheduleData.map((attendee, index) => {
-              const isAvailable =
-                hoveredCell.row !== null &&
-                hoveredCell.column !== null &&
-                attendee.availability[hoveredCell.row][hoveredCell.column] == 1;
-              const opacityStyle = isAvailable
-                ? 'opacity-100'
-                : 'opacity-50 line-through';
-
-              return (
-                <div
-                  key={index}
-                  className={`text-[19px] font-[300] transition duration-300 ${opacityStyle}`}
-                >
-                  {attendee.name}
-                </div>
-              );
-            })}
-          </div>
         </div>
       </div>
+      {selectedCell && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+          <div className="bg-white p-4 rounded-md shadow-md w-[80vw] max-w-md text-center">
+            <h2 className="text-xl font-bold mb-4">Availability Details</h2>
+            <p>
+              Details for cell at row {selectedCell.row} and column{' '}
+              {selectedCell.column}
+            </p>
+            {/* Add your details here */}
+            <button
+              onClick={closeModal}
+              className="mt-4 px-4 py-2 bg-red-500 text-white rounded-md"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
