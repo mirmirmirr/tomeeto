35bd77158c555927cf5486c8c84b06c4655420d7
jzgom067 <jzgombic3@gmail.com>
2024-11-25 23:10:39 -0500

    Add User from_json method
    The first step to getting dashboard events.

--- backend/user.py
+++ backend/user.py
@@ -5,5 +5,11 @@ class User:
     def __init__(self, id: int) -> None:
         self.id: int = id
 
+    @staticmethod
+    def from_json(json: dict) -> "User":
+        if "account_id" in json:
+            return User(json["account_id"])
+        return None
+
     def get_events(self) -> List:
         pass
