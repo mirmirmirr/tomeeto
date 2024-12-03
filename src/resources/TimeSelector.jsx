fe0156d27c82e9196ce82ddea026e4e6289d1184
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2024-12-03 01:08:28 -0500

    1AM + 1PM

--- src/resources/TimeSelector.jsx
+++ src/resources/TimeSelector.jsx
@@ -16,6 +16,7 @@ export default function TimeSelector({
 
   const times = [
     '12:00 AM',
+    '1:00 AM',
     '2:00 AM',
     '3:00 AM',
     '4:00 AM',
@@ -27,6 +28,7 @@ export default function TimeSelector({
     '10:00 AM',
     '11:00 AM',
     '12:00 PM',
+    '1:00 PM',
     '2:00 PM',
     '3:00 PM',
     '4:00 PM',
